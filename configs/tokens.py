c491d7f3ae73108b38f3a009b15cc1f7a1309d1d
Vasu2412028383 <imdigitalvasu+1@gmail.com>
2025-02-15 10:20:06 +0530

    Update tokens.py

--- configs/tokens.py
+++ configs/tokens.py
@@ -1,4 +1,4 @@
 api_hash="acbb4dd6bdec17eab1f4687d1475baa3" # api hask 
 api_id=27283160 #api id
-bot_token = "6272369771:AAHevzJBIJ__aIPNwFP6J1cfoih35PrLKIY"
+bot_token = "7695368751:AAFVrzejlS0BLKZD7Bb1HXXgWl5PGclex_0"
 bin_checkurl = "https://lookup.binlist.net/"
